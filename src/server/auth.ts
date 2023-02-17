0adbff372edcbce3a255cd7019461a584226e297
Rizal Iswandy <=>
2023-02-18 03:16:14 +0800

    coba

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,15 +1,11 @@
-import { GetServerSidePropsContext } from "next";
-import { unstable_getServerSession } from "next-auth";
+import type { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
 
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 
-export const getServerAuthSession = async (ctx: {
+export const getServerAuthSession = (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
-  return await unstable_getServerSession(
-    ctx.req as any,
-    ctx.res as any,
-    authOptions
-  );
+  return getServerSession(ctx.req, ctx.res, authOptions);
 };
