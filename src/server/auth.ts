b19ce7132551c4052720b4428bfef18b4798d441
Rizal Iswandy <=>
2023-02-17 23:18:12 +0800

    adslfasdfasdf; sdaf ;d

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,20 +1,14 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingMessage } from "http";
 import type { GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth";
 
 import authOptions from "../pages/api/auth/[...nextauth]";
 
-type GetServerSessionArgs = [
-  req: IncomingMessage & { cookies: Partial<{ [key: string]: string }> },
-  res: ServerResponse<IncomingMessage>,
-  options: any
-];
-
 export const getServerAuthSession = async (ctx: GetServerSidePropsContext) => {
-  const args: GetServerSessionArgs = [
-    ctx.req as IncomingMessage & { cookies: { [key: string]: string } },
-    ctx.res,
-    authOptions,
-  ];
-  return await unstable_getServerSession(...args);
+  const { req, res } = ctx;
+  return await unstable_getServerSession(
+    req as IncomingMessage & { cookies: { [key: string]: string } },
+    res,
+    authOptions
+  );
 };
