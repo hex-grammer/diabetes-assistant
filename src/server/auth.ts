73e88ed1b250c4ad5a940b72f8665115b3e8be85
Rizal Iswandy <=>
2023-02-18 01:26:27 +0800

    test auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,11 @@
-import { IncomingMessage } from "http";
-import type { GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth";
 
 import authOptions from "../pages/api/auth/[...nextauth]";
+import { IncomingMessage, ServerResponse } from "http";
 
-export const getServerAuthSession = async (ctx: GetServerSidePropsContext) => {
-  const req = ctx.req as IncomingMessage & {
-    cookies: { [key: string]: string };
-  };
-  const res = ctx.res;
-  const session = await unstable_getServerSession(req, res, authOptions as any);
-  return session;
+export const getServerAuthSession = async (ctx: {
+  req: IncomingMessage & { cookies: Partial<{ [key: string]: string }> };
+  res: ServerResponse<IncomingMessage>;
+}) => {
+  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
 };
