c1c88afb0c60899cd343b8f179754bfc69ec5a7b
Rizal Iswandy <=>
2023-02-17 23:09:08 +0800

    auth server error

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,8 +1,13 @@
+import { IncomingMessage } from "http";
 import type { GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth";
 
 import authOptions from "../pages/api/auth/[...nextauth]";
 
 export const getServerAuthSession = async (ctx: GetServerSidePropsContext) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
+  return await unstable_getServerSession(
+    ctx.req as IncomingMessage & { cookies: { [key: string]: string } },
+    ctx.res,
+    authOptions
+  );
 };
