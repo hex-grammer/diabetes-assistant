e89d14758eac7764661ddada117af92bad7f594c
Rizal Iswandy <=>
2023-02-17 23:15:17 +0800

    rerver auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,20 @@
-import { IncomingMessage } from "http";
+import { IncomingMessage, ServerResponse } from "http";
 import type { GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth";
 
 import authOptions from "../pages/api/auth/[...nextauth]";
 
+type GetServerSessionArgs = [
+  req: IncomingMessage & { cookies: Partial<{ [key: string]: string }> },
+  res: ServerResponse<IncomingMessage>,
+  options: any
+];
+
 export const getServerAuthSession = async (ctx: GetServerSidePropsContext) => {
-  return await unstable_getServerSession(
+  const args: GetServerSessionArgs = [
     ctx.req as IncomingMessage & { cookies: { [key: string]: string } },
     ctx.res,
-    authOptions
-  );
+    authOptions,
+  ];
+  return await unstable_getServerSession(...args);
 };
